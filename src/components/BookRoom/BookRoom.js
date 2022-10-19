d0ddf859017eb5f34c47c26a3ca0ffe4e92ad3b4
unknown <julianatonu@gmail.com>
2022-10-19 15:28:12 +0600

    added bookroom detals

--- src/components/BookRoom/BookRoom.js
+++ src/components/BookRoom/BookRoom.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import './BookRoom.css'
+
 
 const BookRoom = () => {
     return (
-        <div>
-            <h1>hey you can book any room now</h1>
+        <div className='container'>
+            <h1 className='my-4 text-center  '>For booking please login yourself first.</h1>
+            
+      
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+  <div className="col">
+    <div className="card">
+      <img src="https://as2.ftcdn.net/v2/jpg/00/48/05/67/1000_F_48056790_Mziu2MGTp8vKnulRp67Jp3vWXpqhV4Gi.jpg" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Single Room</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <button className='btn btn-info'>Book Now</button>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?w=1060&t=st=1666170834~exp=1666171434~hmac=d7fbd315246b84d73b0c47115e608c07e21914deafc3e4011db43b4dc599d7d5" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Couple Room</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <button className='btn btn-info'>Book Now</button>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card">
+      <img src="https://img.freepik.com/premium-photo/hotel-room_321831-251.jpg?w=1060" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Family Room</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <button className='btn btn-info'>Book Now</button>
+      </div>
+    </div>
+  </div>
+  
+</div>
+   
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../UserContext/UserContext';
+// import Carousel from 'react-bootstrap/Carousel'; 
+import './Home.css'
 
 const Home = () => {
     const {user} =useContext(AuthContext)
     return (
-        <div>
-           <h1> This is home</h1>
-           <h2>Welcome : {user?.email}</h2>
+        <div className=' text-center'>
+           
+           <p>Welcome : {user?.email}</p>
+<div>
+    <img src="https://img.freepik.com/free-photo/modern-luxury-bedroom-suite-bathroom_105762-1791.jpg?w=1060&t=st=1666167939~exp=1666168539~hmac=e29222e052eab41c04b30626a0e6b93a56505ad80a398fc85fdfcde3793a24ef" alt="" className="img"  />
+</div>
+           
         </div>
     );
 };
